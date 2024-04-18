38448146d2e59b0b787e2ac99aeab211a2c84b5c
Sajish <shresthasajish7@gmail.com>
2024-04-18 21:56:14 +0545

    double jump roll, wall slide added

---
+++ Assets/NinjaPlatformer/Scripts/MikasaActions.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public struct MikasaActions
+{
+    public bool attack1;
+    public bool attack2;
+    public bool crouchWithoutSword;
+    public bool crouchWSword;
+    public bool dashWithoutSword;
+    public bool idleWithoutSword;
+    public bool jumpAttack;
+    public bool jumpWithoutSword;
+    public bool jumpSkill;
+    public bool spitFireBall;
+    public bool swordIdle;
+    public bool swordInstance;
+    public bool swordRun;
+    public bool swordJump;
+    public bool swordStrike;
+    public bool takeDamage;
+    public bool WalkWithoutSword;
+
+    //// Constructor to initialize the struct with default values
+    //public MikasaActions(bool jump, bool action, bool run)
+    //{
+    //    Jump = jump;
+    //    Action = action;
+    //    Run = run;
+    //}
+
+    //// Method to reset all actions to false
+    //public void ResetActions()
+    //{
+    //    Jump = false;
+    //    Action = false;
+    //    Run = false;
+    //}
+}
+
